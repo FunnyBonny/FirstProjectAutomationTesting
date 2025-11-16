b24811dafb2bf7f153465f358ebf3e0b577f5526
Railean Lia <ceiti.liarailean@gmail.com>
2025-11-16 20:07:14 +0200

    Update Driver.java

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -2,22 +2,29 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
     private static WebDriver driver;
 
+    /**
+     * Returnează o instanță de WebDriver local (Chrome headless pentru CI/CD)
+     */
     public static WebDriver getLocalDriver() {
         if (driver == null) {
             try {
+                // Pregătim driver-ul Chrome
                 WebDriverManager.chromedriver().clearDriverCache().setup();
 
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized");
-                options.addArguments("--remote-allow-origins=*");
 
-                
+                // Opțiuni recomandate pentru rularea pe CI (GitHub Actions)
+                options.addArguments("--headless=new");          // headless Chrome modern
+                options.addArguments("--no-sandbox");            // necesar pe GitHub Actions
+                options.addArguments("--disable-dev-shm-usage"); // rezolvă probleme cu memoria
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--window-size=1920,1080"); // pentru testele care au nevoie de dimensiune
 
                 driver = new ChromeDriver(options);
 
@@ -28,6 +35,9 @@ public class Driver {
         return driver;
     }
 
+    /**
+     * Închide driver-ul și resetează instanța
+     */
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
@@ -35,6 +45,9 @@ public class Driver {
         }
     }
 
+    /**
+     * Resetează driver-ul (închide și recreează la următoarea cerere)
+     */
     public static void resetDriver() {
         quitDriver();
     }
